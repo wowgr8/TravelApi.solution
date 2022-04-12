c0b3bcdb1d966710d5f18529e55190d58c22eddc
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-11 20:35:50 -0700

    add necessary project files
    Co-authored-by: Ebru Rice <ebruerenb@gmail.com>
    Coauthored by:  Wajma Niazi  <w1niazi@hotmail.com>

---
+++ TravelApi/Models/Destination.cs
@@ -0,0 +1,13 @@
+namespace TravelApi.Models
+{
+    public class Destination
+    {
+        public int DestinationId { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
