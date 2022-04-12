0822a3b6f116c29372cdebe40393d07b6a5c1606
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-11 20:38:40 -0700

    add initial migration dquote> Co-authored-by: Ebru Rice <ebruerenb@gmail.com> dquote> Coauthored by:  Wajma Niazi  <w1niazi@hotmail.com>

---
+++ TravelApi/Migrations/20220412032657_initial.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelApi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Destinations",
+                columns: table => new
+                {
+                    DestinationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    City = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Country = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destinations", x => x.DestinationId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Destinations",
+                columns: new[] { "DestinationId", "City", "Country", "Rating" },
+                values: new object[] { 1, "Florence", "Italy", 3 });
+
+            migrationBuilder.InsertData(
+                table: "Destinations",
+                columns: new[] { "DestinationId", "City", "Country", "Rating" },
+                values: new object[] { 2, "Paris", "France", 4 });
+
+            migrationBuilder.InsertData(
+                table: "Destinations",
+                columns: new[] { "DestinationId", "City", "Country", "Rating" },
+                values: new object[] { 3, "Washington DC", "USA", 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Destinations");
+        }
+    }
+}
