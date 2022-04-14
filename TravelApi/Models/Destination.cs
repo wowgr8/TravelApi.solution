331f25d2f73a7f5666081299a468cda18089c7e0
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-13 20:59:51 -0700

    added note for search functionality in controller GET
    Co-Authored-by: Chris Nakayama <cnakayam@gmail.com>
    Co-authored-by: Wajma Niazi <w1niazi@hotmail.com>
    Co-authored-by: Ebru Rice <ebruerenb@gmail.com>

--- TravelApi/Models/Destination.cs
+++ TravelApi/Models/Destination.cs
@@ -7,7 +7,8 @@ namespace TravelApi.Models
         public string City { get; set; }
 
         public string Country { get; set; }
-
+        // [Required]
+        // [Range(0, 10, ErrorMessage = "Age must be between 0 and 10.")]
         public int Rating { get; set; }
     }
 }
