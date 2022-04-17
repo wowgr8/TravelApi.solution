3412ef7419f820f703c63cf89c1982bbfc0dcc2c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-17 15:16:27 -0700

    add Swagger/Swatchbuckle configration
    Co-Authored-by: Chris Nakayama <cnakayam@gmail.com>
    Co-Authored-by: Amber Wilwand <amoothenielson@gmail.com>
    Co-authored-by: Ebru Rice <ebruerenb@gmail.com>
    Co-authored-by: Wajma Niazi <w1niazi@hotmail.com>

--- TravelApi/Startup.cs
+++ TravelApi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using TravelApi.Models;
+using Microsoft.OpenApi.Models;
 
 namespace TravelApi
 {
@@ -24,6 +25,30 @@ namespace TravelApi
             services.AddDbContext<TravelApiContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+
+            // registered the Swagger generator, defining one or more Swagger documents.
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+            {
+                Title = "TravelApi",
+                Version = "v1", 
+                Description = "A simple yet functional of an Travel ASP.NET Core Web API",
+                // Contact = new OpenApiContact
+                // {
+                // Name = "Erika Debelis",
+                // Email = "erika.debelis@gmail.com",
+                // Url = new Uri("https://www.linkedin.com/in/erika-debelis/")
+                // },
+                // License = new OpenApiLicense
+                // {
+                // Name = "Use under AFL",
+                // Url = new Uri("https://opensource.org/licenses/AFL-3.0"),
+                // }
+            });
+        });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +57,17 @@ namespace TravelApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                // exposed the generated Swagger as JSON endpoint(s) using middleware
+                app.UseSwagger(c =>
+                {
+                    c.SerializeAsV2 = true;
+                });
+                    // inserted the swagger-ui middleware to expose interactive documentation, specifying the Swagger JSON endpoint(s) to power it from
+                    app.UseSwaggerUI(c => 
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Destinations v1");
+                });
             }
 
             // app.UseHttpsRedirection();
